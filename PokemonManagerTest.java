ac0bcf24429eb21fe3e77ad77fff473d6b3f3055
Luis <langelgirona@gmail.com>
2025-03-13 21:39:12 -0600

    Agregar documentación a la clase de pruebas unitarias PokemonManagerTest

--- PokemonManagerTest.java
+++ PokemonManagerTest.java
@@ -5,15 +5,32 @@ import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Clase de pruebas unitarias para la clase PokemonManager.
+ * Proporciona métodos para verificar las operaciones de gestión de Pokémon.
+ * 
+ * @Project : Hoja de Trabajo 6 - Algoritmos y Estructura de Datos
+ * @Author : Luis Angel Girón Arévalo
+ * Creación : 13/03/2025
+ * Última modificación : 13/03/2025
+ * @File Name: PokemonManagerTest.java
+ */
 class PokemonManagerTest {
     private PokemonManager<Pokemon> manager;
 
+    /**
+     * Configuración inicial antes de cada prueba.
+     * @throws IOException Si ocurre un error al cargar los datos de los Pokémon.
+     */
     @BeforeEach
     void setUp() throws IOException {
         manager = new PokemonManager<>("hashmap");
         manager.loadPokemons("pokemon_data_pokeapi.csv", Pokemon::new);
     }
 
+    /**
+     * Prueba la adición de un Pokémon a la colección del usuario.
+     */
     @Test
     void testAddPokemonToCollection() {
         manager.addPokemonToCollection("Bulbasaur");
@@ -21,12 +38,18 @@ class PokemonManagerTest {
         assertTrue(manager.getUserCollection().contains(manager.getPokemonMap().get("Bulbasaur")));
     }
 
+    /**
+     * Prueba la visualización de los datos de un Pokémon.
+     */
     @Test
     void testShowPokemon() {
         manager.showPokemon("Bulbasaur");
         assertEquals("Bulbasaur", manager.getPokemonMap().get("Bulbasaur").name);
     }
 
+    /**
+     * Prueba la visualización de los Pokémon de la colección del usuario ordenados por tipo1.
+     */
     @Test
     void testShowUserPokemonsByType() {
         manager.addPokemonToCollection("Bulbasaur");
@@ -36,6 +59,9 @@ class PokemonManagerTest {
         assertEquals("Charmander", manager.getUserCollection().get(1).name);
     }
 
+    /**
+     * Prueba la visualización de los Pokémon que tienen una habilidad específica.
+     */
     @Test
     void testShowPokemonsByAbility() {
         manager.showPokemonsByAbility("Overgrow");
