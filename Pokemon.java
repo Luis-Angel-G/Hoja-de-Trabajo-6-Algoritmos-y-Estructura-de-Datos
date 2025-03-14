04d0a7a2bac606aef7b55bcadd7348459c919dc8
Luis <langelgirona@gmail.com>
2025-03-13 21:35:28 -0600

    Agregar documentación a la clase Pokemon y sus constructores

--- Pokemon.java
+++ Pokemon.java
@@ -1,9 +1,32 @@
+/**
+ * Clase que representa un Pokémon con sus atributos y métodos relacionados.
+ * Proporciona métodos para gestionar los datos de un Pokémon y generar su representación en cadena.
+ * 
+ * @Project : Hoja de Trabajo 6 - Algoritmos y Estructura de Datos
+ * @Author : Luis Angel Girón Arévalo
+ * Creación : 13/03/2025
+ * Última modificación : 13/03/2025
+ * @File Name: Pokemon.java
+ */
 public class Pokemon {
     String name, type1, type2, classification, abilities;
     double height, weight;
     int generation;
     boolean legendary;
 
+    /**
+     * Constructor de la clase Pokemon que inicializa los atributos con los valores proporcionados.
+     * 
+     * @param name El nombre del Pokémon.
+     * @param type1 El tipo primario del Pokémon.
+     * @param type2 El tipo secundario del Pokémon.
+     * @param classification La clasificación del Pokémon.
+     * @param height La altura del Pokémon en metros.
+     * @param weight El peso del Pokémon en kilogramos.
+     * @param abilities Las habilidades del Pokémon.
+     * @param generation La generación del Pokémon.
+     * @param legendary Indica si el Pokémon es legendario.
+     */
     public Pokemon(String name, String type1, String type2, String classification, double height, double weight, String abilities, int generation, boolean legendary) {
         this.name = name;
         this.type1 = type1;
@@ -16,6 +39,11 @@ public class Pokemon {
         this.legendary = legendary;
     }
 
+    /**
+     * Constructor de la clase Pokemon que inicializa los atributos a partir de un array de datos.
+     * 
+     * @param data Un array de cadenas que contiene los datos del Pokémon.
+     */
     public Pokemon(String[] data) {
         this.name = data[0].trim();
         this.type1 = data[2].trim();
@@ -28,10 +56,20 @@ public class Pokemon {
         this.legendary = data[9].trim().equalsIgnoreCase("Yes");
     }
 
+    /**
+     * Obtiene las habilidades del Pokémon.
+     * 
+     * @return Las habilidades del Pokémon.
+     */
     public String getAbilities() {
         return abilities;
     }
 
+    /**
+     * Genera una representación en cadena del Pokémon con todos sus atributos.
+     * 
+     * @return Una cadena que representa al Pokémon.
+     */
     @Override
     public String toString() {
         return name + " - Tipo1: " + type1 + " - Tipo2: " + type2 + " - Clasificación: " + classification + " - Altura: " + height + "m - Peso: " + weight + "kg - Habilidades: " + abilities + " - Generación: " + generation + " - Legendario: " + (legendary ? "Sí" : "No");
